1d0f20a2f21b49e44dca57947aa5ba00d42bca1d
Aamr-mohamed <110783762+Aamr-mohamed@users.noreply.github.com>
2023-07-11 13:42:06 +0300

    -updates
    reorganized the routes and added the error page and fixed the navigation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import LoginForm from "./routes/login";
+import SignupForm from "./routes/signup";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "login/",
+    element: <LoginForm />,
+  },
+  {
+    path: "signup/",
+    element: <SignupForm />,
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
@@ -15,3 +34,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// export default App;
---
+++ src/routes/root.jsx
@@ -0,0 +1,11 @@
+export default function Root() {
+  return (
+    <div>
+        <li>
+            <a href={`/login`}>Login</a>
+        </li>
+        <li>
+            <a href={`/signup`}>Signup</a>
+        </li>
+    </div>
+);}
