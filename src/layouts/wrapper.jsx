8f554b800da31bba48aebd0a546bbf62c29f19ad
Amr-Mohamed <amr1236661@gmail.com>
2023-11-03 21:05:11 +0200

    changing homepage from bootstrap to tailwind

--- src/layouts/wrapper.jsx
+++ src/layouts/wrapper.jsx
@@ -5,17 +5,26 @@ import { Col, Container, Row } from "react-bootstrap";
 
 function Wrapper({ children }) {
   return (
-    <Container fluid>
-      <Row>
-        <Col lg={12}>
-          <Header />
-        </Col>
-        <Col lg={2}>
-          <SideBar />
-        </Col>
-        <Col lg={10}>{children}</Col>
-      </Row>
-    </Container>
+    // <Container fluid>
+    //   <Row>
+    //     <Col lg={12}>
+    //       <Header />
+    //     </Col>
+    //     <Col lg={2}>
+    //       <SideBar />
+    //     </Col>
+    //     <Col lg={10}>{children}</Col>
+    //   </Row>
+    // </Container>
+    <div className="h-screen grid grid-cols-4">
+      <div className="col-span-4">
+        <Header />
+      </div>
+      <div className="pl-8">
+        <SideBar />
+      </div>
+      <div className="">{children}</div>
+    </div>
   );
 }
 
